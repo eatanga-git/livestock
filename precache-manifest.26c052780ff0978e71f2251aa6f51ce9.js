3ba635cf33cc49ed6d4802ca05946f077400dcc1
Erick <atangaerick@gmail.com>
2019-09-18 22:50:38 -0400

    Updates

---
+++ precache-manifest.26c052780ff0978e71f2251aa6f51ce9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62e68eac4421f01a78b1ed2b61bb687e",
+    "url": "/eatanga-git/livestock.git/index.html"
+  },
+  {
+    "revision": "19714e4f6709a0961a37",
+    "url": "/eatanga-git/livestock.git/static/css/main.0f209de8.chunk.css"
+  },
+  {
+    "revision": "368d834e8a7ea965bf93",
+    "url": "/eatanga-git/livestock.git/static/js/2.b299845c.chunk.js"
+  },
+  {
+    "revision": "19714e4f6709a0961a37",
+    "url": "/eatanga-git/livestock.git/static/js/main.184ed104.chunk.js"
+  },
+  {
+    "revision": "1a0642b923838255b1fb",
+    "url": "/eatanga-git/livestock.git/static/js/runtime~main.c202af45.js"
+  }
+]);
